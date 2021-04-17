859d949d443d20e48a59321637d3eedc118276e9
Glitch (grove-fierce-regnosaurus) <none>
2021-04-17 07:16:20 +0000

    👼❤️ Updated with Glitch

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", (req, res) => {
+  console.log(req.socket.remoteAddress)
+  res.json({ipaddress: req.socket.remoteAddress, language: req.headers['accept-language'], software: req.headers['user-agent']});
 });
 
 
